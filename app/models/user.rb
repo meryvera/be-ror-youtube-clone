8333b25e259cbafefd9c4f013619eeb2e91666b2
mery vera <meryvera@merys-MacBook-Pro.local>
2024-02-04 17:53:54 -0500

    setting default_avatar.png in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+    # Callback para establecer un avatar predeterminado antes de guardar el usuario
+    before_save :set_default_avatar
+
+    private
+  
+    # Método para establecer el avatar predeterminado
+    def set_default_avatar
+      self.avatar ||= 'default_avatar.png'
+    end
 end
