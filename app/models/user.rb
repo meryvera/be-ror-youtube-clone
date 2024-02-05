b8f950e34086f6a9d2de955b9621338447fb3a65
mery vera <meryvera@merys-MacBook-Pro.local>
2024-02-04 19:40:46 -0500

    User model association modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-    # Callback para establecer un avatar predeterminado antes de guardar el usuario
-    before_save :set_default_avatar
 
-    private
-  
-    # Método para establecer el avatar predeterminado
-    def set_default_avatar
-      self.avatar ||= 'default_avatar.png'
-    end
+  # Associations
+  has_many :videos
+  has_many :comments
+  has_many :like_dislikes
+
+  # Callback para establecer un avatar predeterminado antes de guardar el usuario
+  before_save :set_default_avatar
+
+  private
+
+  # Método para establecer el avatar predeterminado
+  def set_default_avatar
+    self.avatar ||= 'default_avatar.png'
+  end
 end
