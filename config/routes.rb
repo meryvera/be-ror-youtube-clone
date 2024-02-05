c607eda45acf78dbdfbfb801c42651f6624fc42d
mery vera <meryvera@merys-MacBook-Pro.local>
2024-02-04 22:53:48 -0500

    routes config for entities and auth/login done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,50 @@
 Rails.application.routes.draw do
+  # Rutas para Devise
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Namespace para la API versión 1
+  namespace :api do
+    namespace :v1 do
+      # Rutas para la autenticación JWT
+      post 'auth/login', to: 'authentication_jwt#login'
+      # delete 'auth/logout', to: 'authentication_jwt#logout'  # Si tienes una acción de logout
+ 
+
+      # Rutas para Usuarios
+      resources :users, only: %i[index show create update destroy] do
+        # Rutas anidadas para Videos bajo Usuarios
+        resources :videos, only: %i[index show create update destroy]
+
+        # Rutas anidadas para Comentarios bajo Usuarios
+        resources :comments, only: %i[index show create update destroy]
+
+        # Rutas anidadas para LikeDislikes bajo Usuarios
+        resources :like_dislikes, only: %i[index create destroy]
+      end
+
+      # Rutas para Videos
+      resources :videos, only: %i[index show create update destroy] do
+        # Rutas anidadas para Comentarios bajo Videos
+        resources :comments, only: %i[index show create update destroy]
+
+        # Rutas anidadas para LikeDislikes bajo Videos
+        resources :like_dislikes, only: %i[index create destroy]
+      end
+
+      # Rutas para Comentarios
+      resources :comments, only: %i[index show create update destroy] do
+        # Rutas anidadas para LikeDislikes bajo Comentarios
+        resources :like_dislikes, only: %i[index create destroy]
+      end
+
+      # Rutas para LikeDislikes
+      resources :like_dislikes, only: %i[index create destroy]
+    end
+  end
+
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Puedes definir aquí una ruta raíz si es necesario
+  # root "main#index"
 end
