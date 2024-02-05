8afce28d8347626df1f9ab5603532df38728760a
mery vera <meryvera@merys-MacBook-Pro.local>
2024-02-04 19:33:43 -0500

    Comment model modified

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :video
+  has_many :like_dislikes, as: :like_dislike_model
 end
--- db/migrate/20240204231852_create_comments.rb
+++ db/migrate/20240204231852_create_comments.rb
@@ -1,11 +1,14 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.text :content
+      t.text :content, limit: 500, null: false
       t.references :user, null: false, foreign_key: true
       t.references :video, null: false, foreign_key: true
 
       t.timestamps
     end
+    # Agregar índices
+    add_index :comments, :user_id
+    add_index :comments, :video_id
   end
 end
