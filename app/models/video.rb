fa3ef545e84bff71d3f56b724039c3697e4eef73
mery vera <meryvera@merys-MacBook-Pro.local>
2024-02-04 19:59:27 -0500

    Revert "Video model modified"
    This reverts commit 60f5abd4b4b72fe14e93ef56d219d7cc4ab96db1.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,3 @@
 class Video < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :like_dislikes, as: :like_dislike_model
 end
--- db/migrate/20240204231316_create_videos.rb
+++ db/migrate/20240204231316_create_videos.rb
@@ -1,14 +1,14 @@
 class CreateVideos < ActiveRecord::Migration[7.1]
   def change
     create_table :videos do |t|
-      t.string :title, null: false
-      t.text :description, limit: 200, null: false
-      t.string :url_video, null: false
+      t.string :title
+      t.text :description
+      t.string :url_video
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
     # Agregar índices
-    add_index :videos, :user_id
+    # add_index :videos, :user_id
   end
 end
